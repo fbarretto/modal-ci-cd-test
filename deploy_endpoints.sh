706ac01c03ff00c3b65c2da2c9730d8954a135d4
barretto <b4rretto@gmail.com>
2024-11-09 12:35:57 -0500

    Refactor ci-cd.yml and deploy_endpoints.sh to pass branch name for deployment and enhance change detection logic

--- deploy_endpoints.sh
+++ deploy_endpoints.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-# Fetch the latest changes on the main branch
-git fetch origin main --depth=2
+# Get the branch name from the first argument
+BRANCH_NAME=$1
+# Echo the branch name
+echo "Branch name: $BRANCH_NAME"
+# Set branch name to "sandbox" for debugging purposes
+# Fetch the latest changes on the specified branch
+git fetch origin $BRANCH_NAME --depth=2
 
-# Check for changes in project-a directory
-if [ $(git diff HEAD~ --name-only --relative=project-a | wc -l) -gt 0 ]; then
-    echo "Changes detected in project-a. Deploying endpoint-a.py..."
-    modal deploy --env=sandbox project-a/endpoint-a.py
-else
-    echo "No changes detected in project-a. Skipping deployment for endpoint-a."
-fi
+BRANCH_NAME="sandbox"
+# Get the current commit hash
+COMMIT_HASH=$(git rev-parse HEAD)
 
-# Check for changes in project-b directory
-if [ $(git diff HEAD~ --name-only --relative=project-b | wc -l) -gt 0 ]; then
-    echo "Changes detected in project-b. Deploying endpoint-b.py..."
-    modal deploy --env=sandbox project-b/endpoint-b.py
-else
-    echo "No changes detected in project-b. Skipping deployment for endpoint-b."
-fi
+# # Set the environment based on the branch name
+# if [ "$BRANCH_NAME" == "main" ]; then
+#     ENV="main"
+# else
+#     ENV="dev"
+# fi
+
+# Loop through each directory in the src folder
+for dir in src/*/; do
+    dir=${dir%*/}
+    dir_name=${dir##*/}
+    
+    # Check for changes in the directory
+    if [ $(git diff HEAD~ --name-only --relative=$dir | wc -l) -gt 0 ]; then
+        deploy_file=$(find $dir -maxdepth 1 -name "deploy*.py" | head -n 1)
+        if [ -z "$deploy_file" ]; then
+            echo "Error: No deploy file found in $dir_name. Skipping deployment."
+            continue
+        fi
+        echo "Changes detected in $dir_name. Deploying $deploy_file..."
+        modal deploy --env=$BRANCH_NAME --tag=$COMMIT_HASH $deploy_file
+    else
+        echo "No changes detected in $dir_name. Skipping deployment for $dir_name."
+    fi
+done
